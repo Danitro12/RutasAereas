4f580ba7736bdb21a1ac98d85dbadddb73ba9a7b
Danitro12 <149191551+Danitro12@users.noreply.github.com>
2023-12-20 23:02:17 +0100

    Update Pais.h

--- RutasAereas/include/Pais.h
+++ RutasAereas/include/Pais.h
@@ -1,53 +1,25 @@
-#ifndef PFINAL_PAIS_H
-#define PFINAL_PAIS_H
-
-
 #include "Punto.h"
+#include <string>
 #include <cstring>
-
-class Pais {
-private:
+class Pais{
+  private:
     Punto p;
     string pais;
     string bandera;
+    
+  public:
+    Pais();
+    Punto GetPunto()const;
+    string GetPais()const;
+    string GetBandera()const;
+    void SetPunto(Punto p);
+    void SetBandera(const string &bd){this->bandera = bd;}
+    void SetPais(const string &ps){this->pais = ps;}
+    
+    bool operator<(const Pais &P)const;
+    bool operator==(const Pais &P)const;
+    bool operator==(const Punto &P)const;
 
-public:
-    Pais() = default;
-
-    Punto GetPunto() const { return p; }
-
-    string GetPais() const { return pais; }
-
-    string GetBandera() const { return bandera; }
-
-    bool operator<(const Pais &P) const {
-        return pais < P.pais;
-    }
-
-    bool operator==(const Pais &P) const {
-        return pais == P.pais;
-    }
-
-    bool operator==(const Punto &P) const {
-        return p == P;
-    }
-
-    friend istream &operator>>(istream &is, Pais &P) {
-        double lat, lng;
-
-        is >> lat >> lng >> P.pais >> P.bandera;
-
-        // Modificado
-        P.p = Punto(lat, lng);
-        // P.p=Punto(lat,lng,"");
-        return is;
-    }
-
-    friend ostream &operator<<(ostream &os, const Pais &P) {
-        os << P.p << " " << P.pais << " " << P.bandera << endl;
-        return os;
-    }
 };
-
-
-#endif //PFINAL_PAIS_H
+istream & operator>>(istream & is, Pais & P);
+ostream & operator<<(ostream & os, const Pais &P);
