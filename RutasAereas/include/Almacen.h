d2a0a845bc6052b59d87a7c9e2255aa415becd70
Danitro12 <149191551+Danitro12@users.noreply.github.com>
2023-12-21 00:01:28 +0100

    Update Almacen.h

--- RutasAereas/include/Almacen.h
+++ RutasAereas/include/Almacen.h
@@ -1,71 +1,104 @@
-/**
- * @file Almacen.h
- * @authors Antonio Luzón Ventura y Emilio Román Núñez Hurtado
- */
-
-#ifndef PROYECTOFINAL_ALMACEN_H
-#define PROYECTOFINAL_ALMACEN_H
-
-#include <utility>
-#include <vector>
 #include "Ruta.h"
+#include <map>
 
-using namespace std;
+#ifndef UNTITLED2_ALMACEN_H
+#define UNTITLED2_ALMACEN_H
 
-class Almacen {
+
+
+class Almacen{
 private:
-    vector<Ruta> rutas;
+    map<string,Ruta> rutas;
 
 public:
+    Almacen();
+    void add(const Ruta &ruta);
+    bool find(const Ruta &ruta);
+    Ruta get(const string &a);
+    int size()const{return rutas.size();}
+    class iterator{
+    private:
+        map<string, Ruta>::iterator p;
+    public:
+        iterator() = default;
+        iterator &operator=(const iterator it){
+            p = it.p;
+            return *this;
+        }
+        bool operator==(const iterator it){
+            return p == it.p;
+        }
+        bool operator!=(const iterator it){
+            return p != it.p;
+        }
+        iterator &operator++(){
+            ++p;
+            return *this;
+        }
+        iterator &operator--(){
+            --p;
+            return *this;
+        }
+        const pair<string,Ruta> &operator*()const{
+            return *p;
+        }
+        friend class Almacen;
+        friend class const_iterator;
+    };
 
-    /**
-     * @brief Constructor por defecto
-     */
-    Almacen() = default;
+    class const_iterator{
+    private:
+        map<string,Ruta>::const_iterator p;
+    public:
+        const_iterator() = default;
+        const_iterator &operator=(const const_iterator it){
+            p = it.p;
+            return *this;
+        }
+        const_iterator &operator++(){
+            ++p;
+            return *this;
+        }
+        const_iterator &operator--(){
+            --p;
+            return *this;
+        }
+        bool operator==(const const_iterator it){
+            return p == it.p;
+        };
+        bool operator!=(const const_iterator it){
+            return p != it.p;
+        }
+        const pair<string,Ruta> &operator*()const{
+            return *p;
+        }
 
-    /**
-     * @brief Constructor
-     * @param vrutas
-     */
-    explicit Almacen(vector<Ruta> vrutas) : rutas(std::move(vrutas)) {}
-
-    /**
-     * @brief Constructor de copia
-     * @param A
-     */
-    Almacen(Almacen &A);
-
-    /**
-     *
-     * @param r
-     */
-    void introducir(const Ruta &r);
-
-    /**
-     * @brief Operador de salida de datos
-     * @param os
-     * @param A
-     * @return
-     */
-    friend ostream &operator<<(ostream &os, const Almacen &A);
-
-    /**
-     * @brief Operador de salida de datos
-     * @param is
-     * @param A
-     * @return
-     */
-    friend istream &operator>>(istream &is, Almacen &A);
+        friend class Almacen;
+    };
+    iterator begin(){
+        iterator it;
+        it.p = rutas.begin();
+        return it;
+    }
+    iterator end(){
+        iterator it;
+        it.p = rutas.end();
+        return it;
+    }
+    const_iterator begin()const{
+        const_iterator it;
+        it.p = rutas.begin();
+        return it;
+    }
+    const_iterator end()const{
+        const_iterator it;
+        it.p = rutas.end();
+        return it;
+    }
 
 
-    /**
-     * @brief Comprueba si la ruta dada como parámetro se encuentra en el almacén de rutas
-     * @param R ruta a buscar en el almacén
-     * @return Verdadero o falso
-     */
-    bool contieneRuta(const Ruta &R);
 
 };
-
-
-#endif //PROYECTOFINAL_ALMACEN_H
+ostream &operator<<(ostream &os, const Almacen &R);
+istream &operator>>(istream &is, Almacen &R);
+#endif //UNTITLED2_ALMACEN_H
